7020a82b23599d1f7c3dfe8b2eb3bab3443b106b
bbumblebe <musweugeoffrey@gmail.com>
2023-05-15 06:46:30 -0700

    10001

---
+++ newcode.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+#include "shell.h"
+/**
+ * shell_1 - displays prompts and handles errors
+ * Return: 0
+ */
+int main(void)
+{
+	char **envp = malloc(sizeof(char *) * (env_count + 1));
+
+	line = NULL;
+	size = 0;
+	env_count = 0;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		if (getline(&line, &size, stdin) == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = '\0';
+		av[0] = line;
+		pid = fork();
+
+		if (pid == -1)
+			perror("fork");
+		else if (pid == 0)
+		{
+			execve(av[0], av, envp);
+			perror("execve");
+			exit(1);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	}
+	free(line);
+	return (0);
+}

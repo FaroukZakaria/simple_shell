7ba5754e271053791bb9853c4adf62960d7328c1
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-16 12:31:40 +0000

    task answer

--- newcode.c
+++ newcode.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <errno.h>
 #include "shell.h"
 /**
  * main - displays prompts and handles errors
--- shell.h
+++ shell.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 char *get_input(void);
 size_t _strlen(const char *str);

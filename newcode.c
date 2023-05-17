bd63a4bbe9718b364d5484a1b4632b6e2f617362
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-17 12:24:35 +0000

    task answer

--- newcode.c
+++ newcode.c
@@ -5,8 +5,6 @@
  */
 int main(void)
 {
-	int status;
-
 	line = NULL;
 	size = 0;
 
@@ -21,19 +19,22 @@ int main(void)
 		av[0] = line;
 		__exit();
 		token();
-		pid = fork();
-		if (pid == -1)
-			perror("fork");
-		else if (pid == 0)
+		if (access(_bin(av[0]), X_OK) == 0)
 		{
-			if (_strcmp(av[0], "env") == 0)
-				execve("/bin/env", av, environ);
-			(execve(_bin(av[0]), av, environ));
-			perror(av[0]);
-			exit(1);
+			pid = fork();
+			if (pid == -1)
+				perror("fork");
+			else if (pid == 0)
+			{
+				if (_strcmp(av[0], "env") == 0)
+					execve("/bin/env", av, environ);
+				(execve(_bin(av[0]), av, environ));
+				perror(av[0]);
+				exit(1);
+			}
+			else
+				waitpid(pid, &status, 0);
 		}
-		else
-			waitpid(pid, &status, 0);
 	}
 	free(line);
 	return (0);

58573d0b49b24d119ce3e79e57a3541268efc114
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-22 21:55:37 +0000

    task answer

--- sep.c
+++ sep.c
@@ -20,27 +20,35 @@ void sep(char **str, char **_environ)
 		return;
 	while (l < k)
 	{
+		/*printf("current value of l is: %d\n", l);*/
+		/*printf("value of i is: %d\n", i);*/
 		str2 = malloc(sizeof(char *) * i);
 		for (j = 0; _strcmp(str[j], ";") != 0; j++)
 		{
 			str2[j] = malloc(sizeof(char) * (_strlen(str[j])));
 			_strcpy(str2[j], str[j]);
 		}
-		str3 = malloc(sizeof(char *) * ((i - j) + 1));
+		/*printf("j before i add is: %d\n", j);*/
+		j++;
+		str3 = malloc(sizeof(char *) * ((i - j)));
 		for (m = 0; j < i; j++, m++)
 		{
-			str3[m] = str[j];
+			str3[m] = malloc(sizeof(char) * (_strlen(str[j])));
+			_strcpy(str3[m], str[j]);
 			free(str2[j]);
 		}
 		str2[j] = NULL;
 		str3[m] = NULL;
-		free(str);
-		str = malloc(sizeof(str3));
+		/**for (i = 0; str3[i] != NULL; i++)
+			printf("str3 is: %s\n", str3[i]);*/
 		str = str3;
 		l++;
+		/**for (i = 0; str2[i] != NULL; i++)
+			printf("to be executed %s\n", str2[i]);*/
 		pid = fork();
 		if (pid == 0)
 		{
+			printf("GONNA EXECUTE NOW!!!\n");
 			execve(_bin(str2[0]), str2, _environ);
 			perror("STUPID");
 			exit(1);
@@ -50,8 +58,10 @@ void sep(char **str, char **_environ)
 		else
 		{
 			waitpid(pid, &status, 0);
+			printf("does this even execute?");
 		}
 	}
 	free(str2);
+	free(str3);
 	return;
 }

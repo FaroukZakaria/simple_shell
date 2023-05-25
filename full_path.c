762f95e1e6390e0cfd8f9462c153f3c87894bc4c
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-25 01:20:13 +0000

    task answer

--- full_path.c
+++ full_path.c
@@ -10,17 +10,19 @@ char *_bin(char *str)
 	int str_len = _strlen(str);
 	int prefix_len = _strlen(prefix);
 	char *new_str = NULL;
+	char *new2;
 
 	if (_strncmp(str, prefix, _strlen(prefix)) != 0)
 	{
-		new_str = (char *) malloc(str_len + prefix_len + 1);
+		new_str = (char *)malloc(str_len + prefix_len + 1);
 		_strcpy(new_str, prefix);
 		_strcat(new_str, str);
-		return (new_str);
+		new2 = new_str;
+		free(new_str);
+		return (new2);
 	}
 	else
 	{
-		free(new_str);
 		return (str);
 	}
 }
